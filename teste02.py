9b4e53db23ae71bdcaa26fefdf9c6606be73c1a2
andregtorres <andregtorres@gmail.com>
2017-04-01 21:13:37 +0200

    People now get away from each other

--- teste02.py
+++ teste02.py
@@ -10,50 +10,79 @@ gridResol   = 1.           #grid resolution
 
 class Ppl(object):
     """docstring for Ppl."""
-    def __init__(self, x_=0.5, y_=0.5, a_=1, b_=1):
+    def __init__(self, x_=0.5, y_=0.5, a_=1, b_=1, v_=50):
         super(Ppl, self).__init__()
-        self.x = x_*dimensions[0]
-        self.y = y_*dimensions[1]
+        self.x = int(x_*dimensions[0])
+        self.y = int(y_*dimensions[1])
         self.a = a_
         self.b = b_
+        self.v = v_
     def addToGrid(self):
         for x, l in enumerate(grid):
             dx = abs(x-self.x)
-            if  dx < 0.3*dimensions[0]:
+            if  dx < 0.8*dimensions[0]:
                 for y, c in enumerate(l):
                     dy = abs(y-self.y)
                     d  = np.sqrt(dx**2+dy**2)
-                    if  d < 0.1*dimensions[0]:
-                        if d != 0:
-                            grid[x][y]+=self.a/((self.b*d)**2)
-                        else:
-                            grid[x][y]=grid[x-1][y]
+                    if d > 10:
+                        if not locked[x][y]:
+                            grid[x][y]+=self.a/((d)**2)
+                    else:
+                        if not locked[x][y]:
+                        #locked [x][y] = True
+                            grid[x][y] +=100
+    def takeFromGrid(self):
+        for x, l in enumerate(grid):
+            dx = abs(x-self.x)
+            if  dx < 0.8*dimensions[0]:
+                for y, c in enumerate(l):
+                    dy = abs(y-self.y)
+                    d  = np.sqrt(dx**2+dy**2)
+                    if d > 10:
+                        if not locked[x][y]:
+                            grid[x][y]-=self.a/((d)**2)
+                    else:
+                        if not locked[x][y]:
+                        #locked [x][y] = True
+                            grid[x][y] -=100
+
+    def step(self):
+        self.takeFromGrid()
+        oldX=self.x
+        oldY=self.y
+        self.x=int(round(self.x-gradx[oldX][oldY]*self.v))
+        self.y=int(round(self.y-grady[oldX][oldY]*self.v))
+        self.addToGrid()
 
 Nx      = int((dimensions[0])/gridResol) #number of grid points
 Ny      = int((dimensions[1])/gridResol) #number of grid points
 Npoints     = Nx*Ny
 grid    = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
+locked  = np.zeros(Npoints, dtype=np.bool ).reshape(Nx, Ny)
+gradx   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
+grady   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 print "Nx,Ny= ",Nx," , ",Ny
+time=0
 
-def setLims():
-    for x, l in enumerate(grid):
+def setLims(field_, value1_, value2_):
+    for x, l in enumerate(field_):
         if x <= Nx/2. :
             for y, c in enumerate(l):
                 if y >= x + (Ny+door)/2. or y < -x + (Ny-door)/2.:
-                    grid[x,y]=100
+                    field_[x][y]=value1_
                 if x == 0 and y>= (Ny-door)/2. and y<(Ny+door)/2:
-                    grid[x,y]=-1e8
+                    field_[x][y]=value2_
 
 
 def plot():
     fig = plt.figure()
-    plt.contourf(grid)
+    plt.contourf(np.transpose(grid))
     plt.colorbar()
     plt.grid()
     plt.show()
 
 def plotGrad():
-    fig = plt.figure()
+    fig = plt.figure(1)
     dy,dx= np.gradient(grid)
     skip = (slice(None, None, 10), slice(None, None, 10))
     dx=dx[10:]
@@ -62,21 +91,48 @@ def plotGrad():
     plt.grid()
     plt.show()
 
+def plotGrad2():
+    global gradx, grady, grid
+    fig = plt.figure(2)
+    plt.subplot(3, 1, 1)
+    plt.plot(grid[300])
+
+    plt.subplot(3, 1, 2)
+    plt.plot(gradx[300])
+
+    plt.subplot(3, 1, 3)
+    plt.plot(grady[300])
+
+    plt.grid()
+    plt.show()
+
+
+def timeStep(dt_):
+    global time, gradx, grady
+    time+=dt_
+    gradx,grady = np.gradient(grid)
 
 
 
-setLims()
-asdrubal    = Ppl(0.3,0.3, 1e-10, 5e-8)
-anibal      = Ppl(0.4,0.4, 1e-10, 5e-8)
+setLims(grid,100, -100)
+#setLims(gradx,100, -100)
+#setLims(grady,100, -100)
+setLims(locked, True, True)
+
+asdrubal    = Ppl(0.3,0.3, 1e4, 5e-8,20)
+anibal      = Ppl(0.3,0.34, 1e4, 5e-8,20)
+amilcar      = Ppl(0.32,0.32, 1e4, 5e-8,20)
 asdrubal.addToGrid()
 anibal.addToGrid()
+amilcar.addToGrid()
+for i in range(5):
+    timeStep(1)
+    asdrubal.step()
+    anibal.step()
+    amilcar.step()
 
+plotGrad2()
 
-print np.gradient(grid)
-plotGrad()
-'''
-fig = plt.figure()
-plt.plot(grid[300])
-plt.grid()
-plt.show()
-'''
+#print np.gradient(grid)
+#plotGrad2()
+plot()
