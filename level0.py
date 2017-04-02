ea202ac623b4fd729e9a8535e22f1ae2b32562d8
andregtorres <andregtorres@gmail.com>
2017-04-02 20:16:59 +0200

    Simulation parameters and variables

---
+++ level0.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+dimensions  = [1000,1000]     #dimentions for box
+door = dimensions[0]/10.  #dimentions for door
+gridResol   = 1.           #grid resolution
+Nx      = int((dimensions[0])/gridResol) #number of grid points
+Ny      = int((dimensions[1])/gridResol) #number of grid points
+Npoints     = Nx*Ny
+grid    = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
+locked  = np.zeros(Npoints, dtype=np.bool ).reshape(Nx, Ny)
+gradx   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
+grady   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
+time=0
