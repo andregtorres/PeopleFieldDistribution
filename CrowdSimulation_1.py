Fix small bugs; Add velocity computation; Calibration

CrowdSimulation_1.py
@@ -21,10 +21,11 @@ nAgents     = 1
 nSteps      = 0
 video       = False
 cpus        = [1,2,3]
-wallPot     =100
+wallPot     = 200
 respawn     = False
 globalJ     =0
 avgRho      =0
+v           = 50
 
 #TERMINAL PARSER
 parser = argparse.ArgumentParser(description='Crowd Simulation 1.')
@@ -43,7 +44,8 @@ parser.add_argument("-R","--respawn", action='store_true',
                     help='Agents respawn')
 parser.add_argument("-p","--potential", type=float,
                     help='Agent potential paramenter')
-
+parser.add_argument("-k","--velocity", type=float,
+                    help='Reference velocity')
 
 args = parser.parse_args()
 if len(sys.argv) < 2:
@@ -65,6 +67,8 @@ if args.respawn:
     respawn = True
 if args.potential:
     l0.a = args.potential
+if args.velocity:
+    v = args.velocity
 
 
 if (verbose > 1) : print "Nx,Ny= ",l0.Nx," , ",l0.Ny
@@ -88,10 +92,10 @@ def SetLims(field_, value1_, value2_):
 
 def setWallField():
     for x, l in enumerate(l0.locked):
-        if (x %50 == 0 and verbose > 1 ): print x
+        if (x %100 == 0 and verbose > 1 ): print x
         for y, point in enumerate(l):
             if point:
-                applyField(x,y,wallPot, 25)
+                applyField(x,y,wallPot, 30)
 
 def applyField(x0,y0,a, ran):
     for x in range(x0-ran,x0 + ran):
@@ -150,15 +154,18 @@ def timeStep():
     if (verbose > 1) : print "\tCOMPUTING NEW COORDINATES"
     q=Queue()
     npeepz=0
+    velocity=0
     for peep in peepz:
         if not peep.safe:
             Process(target=peep.getNewCoordinates, args=(q,npeepz,verbose)).start()
             npeepz+=1
     for i in range(npeepz):
         (putOrder, newX, newY, safe)= q.get()
+        velocity+=(np.sqrt((peepz[putOrder].x-newX)**2+(peepz[putOrder].y-newY)**2)/l0.dt*l0.gridResol)
         peepz[putOrder].x=newX
         peepz[putOrder].y=newY
         peepz[putOrder].safe=safe
+    velocity=velocity/npeepz
 
     #COMPUTE PARAMETERS AND DEAL WITH SAFE
     J=0.
@@ -177,7 +184,8 @@ def timeStep():
     J=J/l0.dt
     l0.JBuffer.append(J)
     l0.rhoBuffer.append(rho)
-    if (verbose > 0) : print "\trho:",rho,"J:", J
+    l0.velocityBuffer.append(velocity)
+    if (verbose > 0) : print "\tv:",velocity,"rho:",rho,"J:", J
 
     #RESET GRID
     l0.grid=blank.copy()
@@ -194,7 +202,7 @@ def addAgent(y1,y2,x1,x2):
         y = random.uniform(y1,y2)
         x = random.uniform(x1,x2)
         if inside(x,y): break
-    v = random.uniform(50,55)
+    #v = random.uniform(50,55)
     peepz.append(Agent(x,y,l0.a,5e-8,v))
     peepz[len(peepz)-1].addToGrid()
     if (verbose > 1) : print "\ti=",peepz[len(peepz)-1].id, "x=",peepz[len(peepz)-1].x, "y=",peepz[len(peepz)-1].y,"v=",v
@@ -205,7 +213,7 @@ psutil.Process(os.getpid()).cpu_affinity(cpus)
 wallField  = np.zeros(l0.Npoints, dtype=np.float64 ).reshape(l0.Nx, l0.Ny)
 SetLims(l0.grid,wallPot, -wallPot)
 SetLims(l0.locked, True, True)
-getDoorField(-wallPot)
+getDoorField(-wallPot/2)
 l0.grid+=l0.doorField
 print "SET WALLFIELD"
 setWallField()
@@ -238,7 +246,7 @@ for i in range(nSteps):
 out.plotGraphs(11,0, True)
 globalJ=globalJ/l0.time
 avgRho=avgRho/nSteps
-print "avgRho=",avgRho,"J=",globalJ
+print "\navgvelocity=",np.mean(l0.velocityBuffer), "avgRho=",avgRho,"J=",globalJ
 #if (verbose > 0) : print "SHOWING PLOTS"
 #out.show([2])
 if video :

agent.py
@@ -20,7 +20,7 @@ class Agent(object):
         self.v = v_/l0.gridResol
         self.grid = np.zeros(l0.Npoints, dtype=np.float64 ).reshape(l0.Nx, l0.Ny)
         self.safe=False
-        self.radius=10./l0.gridResol
+        self.radius=l0.radius/l0.gridResol
     def __del__(self):
         self.grid=None
 
@@ -56,28 +56,26 @@ class Agent(object):
         #SET AFFINITY
         psutil.Process(os.getpid()).cpu_affinity([1,2,3])
 
-        newX=self.x-l0.gradx[int(round(self.x))][int(round(self.y))]*self.v
+        newX=self.x-l0.gradx[int(round(self.x))][int(round(self.y))]*self.v #grad already multiplyes py dt
         newY=self.y-l0.grady[int(round(self.x))][int(round(self.y))]*self.v
 
         #print (np.sqrt((self.x-newX)**2+(self.y-newY)**2)/l0.dt)
 
-        if newX >= l0.Nx or newX < 0:
+        if int(round(newX)) >= l0.Nx or newX < 0:
             if newX < l0.door[0] and newY > l0.door[1]-l0.door[2]/2. + self.radius/2. and newY < l0.door[1] +l0.door[2]/2.- self.radius/2.:
                 self.safe=True
                 self.grid=np.zeros(l0.Npoints, dtype=np.float64 ).reshape(l0.Nx, l0.Ny)
-                self.newX=0
-                self.newY=0
             else:
-                l0.lockPrint.acquire()
+                if (verbose > 1) :l0.lockPrint.acquire()
                 if (verbose > 1) :print "\t\t",self.id,"ERROR getNewCoordinates() out of bounds: newX= ",newX
                 if (verbose > 1) :print "\t\t",self.id,"ERROR getNewCoordinates() out of bounds: newY= ",newY
-                l0.lockPrint.release()
+                if (verbose > 1) :l0.lockPrint.release()
                 newX = self.x #TODO
-        if newY >= l0.Ny or newY < 0:
-            l0.lockPrint.acquire()
+        if int(round(newY)) >= l0.Ny or newY < 0:
+            if (verbose > 1) :l0.lockPrint.acquire()
             if (verbose > 1) :print "\t\t",self.id,"ERROR getNewCoordinates() out of bounds: newX= ",newX
             if (verbose > 1) :print "\t\t",self.id,"ERROR getNewCoordinates() out of bounds: newY= ",newY
-            l0.lockPrint.release()
+            if (verbose > 1) :l0.lockPrint.release()
             newY = self.y  #TODO
 
         q.put((putOrder,newX,newY,self.safe))
@@ -88,7 +86,7 @@ class Agent(object):
         self.computeGrid()
         q.put((putOrder,self.grid))
     def inExitArea(self):
-        ret=np.sqrt((self.x-l0.door[0])**2+((self.y-l0.door[1]))**2)-l0.exitAreaR*1.2
+        ret=np.sqrt((self.x-l0.door[0])**2+((self.y-l0.door[1]))**2)-l0.cm(l0.exitAreaR)-self.radius/2. #corrective factor
         if ret<=0:
             return True
         else:

level0.py
@@ -5,13 +5,13 @@ from multiprocessing import Lock
 def cm(cm): #converts cm to pixels
     return int( cm /gridResol)
 
-dimensions  = [400,400]     #dimentions for box in cm
+dimensions  = [500,350]     #dimentions for box in cm
 gridResol   = 1.           #grid resolution in cm/pixel
 Nx      = int((dimensions[0])/gridResol) #number of grid points
 Ny      = int((dimensions[1])/gridResol) #number of grid points
 Npoints     = Nx*Ny
-door= (0,Ny/2., cm(78))
-a=2000.
+door= (0,Ny/2., cm(72))
+a=8000.
 
 grid    = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 doorField = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
@@ -19,10 +19,12 @@ locked  = np.zeros(Npoints, dtype=np.bool ).reshape(Nx, Ny)
 gradx   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 grady   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 time=0
-exitAreaR   = 30 #cm
+exitAreaR   = 40 #cm
 dt          = 1 #s
+radius      = 18. #cm
 JBuffer=[]
 rhoBuffer=[]
+velocityBuffer=[]
 
 #THREADING
 activeThreads=0

outputs.py
@@ -13,7 +13,7 @@ def plot(field,clip):
     fig = plt.figure()
     #plt.contourf(np.transpose(l0.grid))
     if clip:
-        plt.contourf(np.clip(np.transpose(field),0,l0.a/100.))
+        plt.contourf(np.clip(np.transpose(field),0,l0.a/l0.radius/l0.radius))
     else:
         plt.contourf(np.transpose(field))
     plt.colorbar()
@@ -22,7 +22,7 @@ def plot(field,clip):
 #PLOTS FULL GRID AND SAVES PNG
 def save_plot(name_):
     fig = plt.figure(figsize=(l0.dimensions[0]/100,l0.dimensions[1]/100))
-    plt.contourf(np.clip(np.transpose(l0.grid-l0.doorField),0,l0.a/100.))
+    plt.contourf(np.clip(np.transpose(l0.grid-l0.doorField),l0.a/l0.radius/l0.radius*0.5,l0.a/l0.radius/l0.radius*3))
     plt.colorbar()
     plt.grid()
     plt.axes().set_aspect('equal')
@@ -97,14 +97,14 @@ def plotGraphs(n=5, m=0, save=False):
     if m != 0:
         plt.plot(timeN,movingAverage(l0.JBuffer,n),'k-',timeM,movingAverage(l0.JBuffer,m),'k--',alpha=0.7)
     else:
-        plt.plot(timeN,movingAverage(l0.JBuffer,n))
+        plt.plot(timeN,movingAverage(l0.JBuffer,n),'k-')
     ax=plt.subplot(212)
     ax.set_ylabel(r'$\rho (m^{-2})$')
     ax.set_xlabel("t (s)")
     if m != 0:
         plt.plot(timeN,movingAverage(l0.rhoBuffer,n),'k-',timeM,movingAverage(l0.rhoBuffer,m),'k--',alpha=0.7)
     else:
-        plt.plot(timeN,movingAverage(l0.rhoBuffer,n))
+        plt.plot(timeN,movingAverage(l0.rhoBuffer,n),'k-')
     name="aux/JRho_"+str(n)+"_"+str(m)+".png"
     if save: plt.savefig(name, dpi=100)
 