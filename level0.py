8c6a81e2ee170473dca8c3c954f62d79d39c70b4
andregtorres <andregtorres@gmail.com>
2017-05-29 09:51:52 +0200

    Chage some parameters

--- level0.py
+++ level0.py
@@ -5,7 +5,7 @@ from multiprocessing import Lock
 def cm(cm): #converts cm to pixels
     return int( cm /gridResol)
 
-dimensions  = [500,350]     #dimentions for box in cm
+dimensions  = [730,350]     #dimentions for box in cm
 gridResol   = 1.           #grid resolution in cm/pixel
 Nx      = int((dimensions[0])/gridResol) #number of grid points
 Ny      = int((dimensions[1])/gridResol) #number of grid points
@@ -19,7 +19,7 @@ locked  = np.zeros(Npoints, dtype=np.bool ).reshape(Nx, Ny)
 gradx   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 grady   = np.zeros(Npoints, dtype=np.float64 ).reshape(Nx, Ny)
 time=0
-exitAreaR   = 40 #cm
+exitAreaR   = 50 #cm
 dt          = 1 #s
 radius      = 18. #cm
 JBuffer=[]
