a4f5f57748cd04086e1e7087e331d10583afa565
andregtorres <andregtorres@gmail.com>
2017-05-29 09:55:09 +0200

    Minor tweeks

--- CrowdSimulation_1.py
+++ CrowdSimulation_1.py
@@ -71,9 +71,9 @@ if args.velocity:
     v = args.velocity
 
 
-if (verbose > 1) : print "Nx,Ny= ",l0.Nx," , ",l0.Ny
-if (verbose > 1) : print "time resolution= ",l0.dt, "s"
-if (verbose > 1) : print "time steps", nSteps
+if (verbose > 0) : print "Nx,Ny= ",l0.Nx," , ",l0.Ny
+if (verbose > 0) : print "time resolution= ",l0.dt, "s"
+if (verbose > 0) : print "time steps", nSteps
 
 def SetLims(field_, value1_, value2_):
     for x, l in enumerate(field_):
@@ -95,7 +95,7 @@ def setWallField():
         if (x %100 == 0 and verbose > 1 ): print x
         for y, point in enumerate(l):
             if point:
-                applyField(x,y,wallPot, 30)
+                applyField(x,y,wallPot, int(30))
 
 def applyField(x0,y0,a, ran):
     for x in range(x0-ran,x0 + ran):
@@ -105,8 +105,9 @@ def applyField(x0,y0,a, ran):
                     try:
                         if (not l0.locked[x][y]) :
                             d=np.sqrt((x0-x)**2+(y0-y)**2)
-                            if d > 0:
-                                wallField[x][y]+=a/((d)**2)/(2*ran+1.0)
+                            if d > 0 and d<=ran:
+                                wallField[x][y]+=a*l0.gridResol*l0.gridResol/((ran*l0.gridResol)**2-1.)*(ran**2/d**2-1.)
+                                #wallField[x][y]+=a/((d)**2)/(2*ran+1.0)
 
                     except:
                         pass
@@ -115,7 +116,7 @@ def getDoorField(value2_):
         for y, c in enumerate(l):
             if not l0.locked[x][y]:
                 dDoor= np.sqrt((x-l0.door[0])**2+(y- l0.door[1])**2)
-                l0.doorField[x][y]=(value2_ + dDoor)
+                l0.doorField[x][y]=(value2_ + dDoor*l0.gridResol)
 
 
 
@@ -165,8 +166,6 @@ def timeStep():
         peepz[putOrder].x=newX
         peepz[putOrder].y=newY
         peepz[putOrder].safe=safe
-    velocity=velocity/npeepz
-
     #COMPUTE PARAMETERS AND DEAL WITH SAFE
     J=0.
     rho=0.
@@ -179,9 +178,11 @@ def timeStep():
             del peep
             J+=1.
             globalJ+=1.
-    rho=rho/l0.exitAreaR**2/np.pi/2.*10000.
+    rho=rho/l0.exitAreaR**2/np.pi*2.*10000.
     avgRho+=rho
     J=J/l0.dt
+    if npeepz > 0:
+        velocity=velocity/npeepz
     l0.JBuffer.append(J)
     l0.rhoBuffer.append(rho)
     l0.velocityBuffer.append(velocity)
@@ -218,7 +219,7 @@ l0.grid+=l0.doorField
 print "SET WALLFIELD"
 setWallField()
 print "DONE"
-#out.plotX(wallField, 500)
+
 l0.grid+=wallField
 blank=l0.grid.copy()
 #out.plot(l0.grid, False)
@@ -243,7 +244,8 @@ for i in range(nSteps):
 
 #if (verbose > 0) : print "GENERATING PLOT"
 
-out.plotGraphs(11,0, True)
+out.plotGraphs(31,0, True)
+#out.plotGraphs(21,0, True)
 globalJ=globalJ/l0.time
 avgRho=avgRho/nSteps
 print "\navgvelocity=",np.mean(l0.velocityBuffer), "avgRho=",avgRho,"J=",globalJ
